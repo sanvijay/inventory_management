9d01f91bed8a8500c2f8b8e2b3b094d8600b53a2
Sandeep Vijayakumar <svijayakumar@dataguard.de>
2023-08-16 21:58:28 +0200

    add devise and active admin

---
+++ app/models/user.rb
@@ -0,0 +1,15 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, 
+         :recoverable, :rememberable, :validatable
+
+
+  def self.ransackable_attributes(auth_object = nil)
+    ["created_at", "email", "id", "remember_created_at", "reset_password_sent_at", "updated_at"]
+  end
+end
